1f7fbce2cb70f81f1df359559c41d0da7f66cb03
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-09-07 20:17:24 -0600

    functionality for knowing if a version is under another one or not is created

--- pyv/ver.py
+++ pyv/ver.py
@@ -6,7 +6,7 @@ DEFAULT_V_NUM = 0
 
 
 def normalize(v1, v2):
-    difference = v1.number_count - v2.number_count
+    difference = abs(v1.number_count - v2.number_count)
 
     if v1.number_count > v2.number_count:
         v2.resize(difference)
@@ -17,6 +17,7 @@ def normalize(v1, v2):
 
 
 class PyVersion:
+
     __number_count: int = 0
     __version_numbers: list = None
 
@@ -48,38 +49,49 @@ class PyVersion:
             ...
         return cls(*num_ls)
 
+    def resize(self, ls_sp):
+
+        if -ls_sp > self.__number_count:
+            raise ValueError("the resizing number is out of accepted range")
+
+        if ls_sp >= 0:
+            for index in range(ls_sp):
+                self.__version_numbers.append(DEFAULT_V_NUM)
+        else:
+            # making the number positive to make it easier
+            ls_sp_p = -ls_sp
+            for index in range(self.number_count-1, self.number_count - ls_sp_p-1, -1):
+                self.__version_numbers.pop(index)
+
+        self.__number_count += ls_sp
+
+    def removeRedundant(self, value=0):
+        counter = 0
+
+        for index in range(self.__number_count-1, 0, -1):
+            if self.__version_numbers[index] != value:
+                break
+            counter += 1
+        self.resize(-counter)
+
+    def isunder(self, version, check_num=1):
+        ver_1, ver_2 = normalize(self, version)
+
+        for (v1, v2) in zip(ver_1.version_numbers[: check_num], ver_2.version_numbers[:check_num]):
+            if v1 != v2:
+                return False
+        return True
+
     def __str__(self):
         version = ""
         for num in self.__version_numbers:
             version += SEPERATOR+str(num)
         return version
 
-    def resize(self, ls_sp):
-
-        if -ls_sp > self.__number_count:
-            raise ValueError("the resizing number is out of accepted range")
-
-        self.__number_count += ls_sp
-        if ls_sp >= 0:
-            for index in range(ls_sp):
-                self.__version_numbers.insert(index, DEFAULT_V_NUM)
-        else:
-            for index in range(-ls_sp):
-                self.__version_numbers.pop(DEFAULT_V_NUM)
-
-    def removeRedundant(self, value=0):
-        counter = 0
-
-        for index in range(self.__number_count):
-            if self.__version_numbers[index] != value:
-                break
-            counter += 1
-        self.resize(-counter)
-
     def __eq__(self, version):
         ver_1, ver_2 = normalize(self, version)
 
-        for (v1, v2) in zip(ver_1.__version_numbers, ver_2.version_numbers):
+        for (v1, v2) in zip(ver_1.version_numbers, ver_2.version_numbers):
             if v1 != v2:
                 return False
         return True
@@ -87,19 +99,27 @@ class PyVersion:
     def __ne__(self, version):
         return not self.__eq__(version)
 
+    def __gt__(self, version):
+        ver_1, ver_2 = normalize(self, version)
 
+        for (v1, v2) in zip(ver_1.version_numbers, ver_2.version_numbers):
+            if v1 > v2:
+                return True
 
-a = PyVersion().fromstr("5.3.3")
-b = PyVersion().fromstr("4")
-print(a)
-print(b)
-print(a == b)
-print(a, a.number_count)
-print(b, b.number_count)
-a.removeRedundant()
-b.removeRedundant()
-print(a, a.number_count)
-print(b, b.number_count)
+            elif v1 < v2:
+                return False
+
+    def __ge__(self, version):
+        return self.__eq__(version) or self.__gt__(version)
+
+    def __lt__(self, version):
+        return not self.__gt__(version)
+
+    def __le__(self, version):
+        return self.__eq__(version) or self.__lt__(version)
+
+    def __repr__(self):
+        return "PyVersion"+str(tuple(self.version_numbers))
 
 
 
